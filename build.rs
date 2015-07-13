76b5d5e285d6e60f385001b4cf61116e320afea4
Alex Crichton <alex@alexcrichton.com>
2015-07-13 15:21:13 -0700

    Start adding OSX support

--- build.rs
+++ build.rs
@@ -7,10 +7,24 @@ fn main() {
 
     let mut cfg = gcc::Config::new();
 
+    if target.contains("linux") {
+        cfg.define("LINUX", None);
+    } else if target.contains("darwin") {
+        cfg.define("APPLE", None);
+    } else if target.contains("windows") {
+        cfg.define("WINDOWS", None)
+    } else {
+        panic!("\n\nusing currently unsupported target triple with \
+                stacker: {}\n\n", target);
+    }
+
     if target.starts_with("x86_64") {
         cfg.file("src/arch/x86_64.S");
     } else if target.contains("i686") {
         cfg.file("src/arch/i686.S");
+    } else {
+        panic!("\n\nusing currently unsupported target triple with \
+                stacker: {}\n\n", target);
     }
 
     cfg.include("src/arch").compile("libstacker.a");
