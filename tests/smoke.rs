b81b24c12ea1b080b8bd32f91384ad499867c5e3
Alex Crichton <alex@alexcrichton.com>
2015-07-13 16:07:31 -0700

    Add 32-bit linux/OSX support

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,5 +1,8 @@
 extern crate stacker;
 
+use std::sync::mpsc;
+use std::thread;
+
 extern {
     fn __stacker_black_box(t: *const u8);
 }
@@ -20,4 +23,29 @@ fn deep() {
     }
 
     foo(256 * 1024, &mut []);
+}
+
+#[test]
+#[ignore]
+fn panic() {
+    fn foo(n: usize, s: &mut [u8]) {
+        unsafe { __stacker_black_box(s.as_ptr()); }
+        if n > 0 {
+            stacker::maybe_grow(64 * 1024, 1024 * 1024, || {
+                let mut s = [0u8; 1024];
+                foo(n - 1, &mut s);
+                unsafe { __stacker_black_box(s.as_ptr()); }
+            })
+        } else {
+            panic!("bottom");
+        }
+    }
+
+    let (tx, rx) = mpsc::channel::<()>();
+    thread::spawn(move || {
+        foo(64 * 1024, &mut []);
+        drop(tx);
+    }).join().unwrap_err();
+
+    assert!(rx.recv().is_err());
 }
