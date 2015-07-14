af4ce25b296c5ed3d62773f7c117ced6461c1a6f
Alex Crichton <alex@alexcrichton.com>
2015-07-13 18:53:45 -0700

    Don't rely on morestack as much
    Instead start tracking this lib's stack limit in a TLS var that will be
    initialized from the OS rather than the morestack stack limit.

--- src/lib.rs
+++ src/lib.rs
@@ -22,14 +22,20 @@
 //! });
 //! ```
 
+use std::cell::Cell;
+
 #[allow(improper_ctypes)]
 extern {
+    fn __stacker_morestack_stack_limit() -> usize;
+    fn __stacker_set_morestack_stack_limit(limit: usize);
     fn __stacker_stack_pointer() -> usize;
-    fn __stacker_stack_limit() -> usize;
-    fn __stacker_set_stack_limit(limit: usize);
     fn __stacker_switch_stacks(new_stack: usize,
                                fnptr: *const u8,
                                dataptr: *mut u8);
+}
+
+thread_local! {
+    static STACK_LIMIT: Cell<usize> = Cell::new(guess_os_morestack_stack_limit())
 }
 
 /// Grows the call stack if necessary.
@@ -45,7 +51,8 @@ pub fn maybe_grow<R, F: FnOnce() -> R>(red_zone: usize,
                                        stack_size: usize,
                                        f: F) -> R {
     unsafe {
-        if __stacker_stack_pointer() - __stacker_stack_limit() >= red_zone {
+        let limit = STACK_LIMIT.with(|s| s.get());
+        if __stacker_stack_pointer() - limit >= red_zone {
             f()
         } else {
             grow_the_stack(stack_size, f)
@@ -59,26 +66,46 @@ unsafe fn grow_the_stack<R, F: FnOnce() -> R>(stack_size: usize, f: F) -> R {
         thunk: Option<F>,
         ret: Option<R>,
         new_limit: usize,
-        old_limit: usize,
     }
+
+    // Allocate some new stack for oureslves
     let mut stack = Vec::<u8>::with_capacity(stack_size);
+    let new_limit = stack.as_ptr() as usize + 32 * 1024;
+
+    // Save off the old stack limits
+    let old_morestack_limit = __stacker_morestack_stack_limit();
+    let old_limit = STACK_LIMIT.with(|s| s.get());
+
+    // Prepare stack limits for the stack switch, note that the morestack stack
+    // limit will be set to a real value once we've switched threads.
+    __stacker_set_morestack_stack_limit(0);
+    STACK_LIMIT.with(|s| s.set(new_limit));
+
+    // Set up the arguments and do the actual stack switch.
     let mut cx: Context<F, R> = Context {
         thunk: Some(f),
-        ret: None::<R>,
-        new_limit: stack.as_ptr() as usize + 32 * 1024,
-        old_limit: __stacker_stack_limit(),
+        ret: None,
+        new_limit: new_limit,
     };
-
-    __stacker_set_stack_limit(0);
     __stacker_switch_stacks(stack.as_mut_ptr() as usize + stack_size,
                             doit::<R, F> as usize as *const _,
                             &mut cx as *mut _ as *mut _);
-    __stacker_set_stack_limit(cx.old_limit);
+
+    // Once we've returned reset bothe stack limits and then return value same
+    // value the closure returned.
+    __stacker_set_morestack_stack_limit(old_morestack_limit);
+    STACK_LIMIT.with(|s| s.set(old_limit));
     return cx.ret.unwrap();
 
     unsafe extern fn doit<R, F: FnOnce() -> R>(cx: &mut Context<F, R>) {
-        __stacker_set_stack_limit(cx.new_limit);
+        __stacker_set_morestack_stack_limit(cx.new_limit);
         cx.ret = Some(cx.thunk.take().unwrap()());
-        __stacker_set_stack_limit(0);
+        __stacker_set_morestack_stack_limit(0);
+    }
+}
+
+fn guess_os_morestack_stack_limit() -> usize {
+    unsafe {
+        __stacker_morestack_stack_limit()
     }
 }
