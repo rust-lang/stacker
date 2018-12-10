0a03ce9b29cb51ba1de46d17452d91958fad56a7
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-12-10 13:29:28 +0100

    Implement black box inside test file

--- src/arch/windows.c
+++ src/arch/windows.c
@@ -1,7 +1,5 @@
 #include <windows.h>
 
-void __stacker_black_box() {}
-
 PVOID __stacker_get_current_fiber() {
     return GetCurrentFiber();
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -3,19 +3,18 @@ extern crate stacker;
 use std::sync::mpsc;
 use std::thread;
 
-extern {
-    fn __stacker_black_box(t: *const u8);
-}
+#[inline(never)]
+fn __stacker_black_box(_: *const u8) {}
 
 #[test]
 fn deep() {
     fn foo(n: usize, s: &mut [u8]) {
-        unsafe { __stacker_black_box(s.as_ptr()); }
+        __stacker_black_box(s.as_ptr());
         if n > 0 {
             stacker::maybe_grow(64 * 1024, 1024 * 1024, || {
                 let mut s = [0u8; 1024];
                 foo(n - 1, &mut s);
-                unsafe { __stacker_black_box(s.as_ptr()); }
+                __stacker_black_box(s.as_ptr());
             })
         } else {
             println!("bottom");
@@ -28,12 +27,12 @@ fn deep() {
 #[test]
 fn panic() {
     fn foo(n: usize, s: &mut [u8]) {
-        unsafe { __stacker_black_box(s.as_ptr()); }
+        __stacker_black_box(s.as_ptr());
         if n > 0 {
             stacker::maybe_grow(64 * 1024, 1024 * 1024, || {
                 let mut s = [0u8; 1024];
                 foo(n - 1, &mut s);
-                unsafe { __stacker_black_box(s.as_ptr()); }
+                __stacker_black_box(s.as_ptr());
             })
         } else {
             panic!("bottom");
