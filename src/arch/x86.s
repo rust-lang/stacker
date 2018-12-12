9e0bd130ddfbfb8bef149acd98e7cb27bdb449bb
Simonas Kazlauskas <git@kazlauskas.me>
2018-12-12 17:26:16 +0200

    Add necessary assembler directives for COFF

--- src/arch/x86.s
+++ src/arch/x86.s
@@ -5,26 +5,52 @@
 
 #if CFG_TARGET_OS_darwin || CFG_TARGET_OS_macos || CFG_TARGET_OS_ios
 
-#define GLOBL(fnname) .globl _##fnname
+#define COFFDEF(fnname,argsz)
+#define COFFSCL(sclN)
+#define COFFTYPE(typeN)
+#define COFFENDEF
+
+#define GLOBL(fnname,argsz) .globl _##fnname
 #define TYPE(fnname)
-#define FUNCTION(fnname) _##fnname
+#define FUNCTION(fnname,argsz) _##fnname
+#define SIZE(fnname,endlabel)
+
+#elif CFG_TARGET_OS_windows
+
+#define COFFDEF(fnname,argsz) .def @##fnname##@##argsz
+#define COFFSCL(sclN) .scl sclN
+#define COFFTYPE(typeN) .type typeN
+#define COFFENDEF .endef
+
+#define GLOBL(fnname,argsz) .globl @##fnname##@##argsz
+#define TYPE(fnname)
+#define FUNCTION(fnname,argsz) @##fnname##@##argsz
 #define SIZE(fnname,endlabel)
 
 #else
 
-#define GLOBL(fnname) .globl fnname
+#define COFFDEF(fnname,argsz)
+#define COFFSCL(sclN)
+#define COFFTYPE(typeN)
+#define COFFENDEF
+
+#define GLOBL(fnname,argsz) .globl fnname
 #define TYPE(fnname) .type fnname,@function
-#define FUNCTION(fnname) fnname
+#define FUNCTION(fnname,argsz) fnname
 #define SIZE(fnname,endlabel) .size fnname,endlabel-fnname
 
 #endif
 
 
 
-GLOBL(rust_psm_stack_direction)
+COFFDEF(rust_psm_stack_direction,0)
+COFFSCL(2)
+COFFTYPE(32)
+COFFENDEF
+GLOBL(rust_psm_stack_direction,0)
 .p2align 4
 TYPE(rust_psm_stack_direction)
-FUNCTION(rust_psm_stack_direction):
+FUNCTION(rust_psm_stack_direction,0):
 /* extern "fastcall" fn() -> u8 (%al) */
 .cfi_startproc
     movb $STACK_DIRECTION_DESCENDING, %al # always descending on x86_64
@@ -34,10 +60,14 @@ SIZE(rust_psm_stack_direction,.rust_psm_stack_direction_end)
 .cfi_endproc
 
 
-GLOBL(rust_psm_stack_pointer)
+COFFDEF(rust_psm_stack_pointer,0)
+COFFSCL(2)
+COFFTYPE(32)
+COFFENDEF
+GLOBL(rust_psm_stack_pointer,0)
 .p2align 4
 TYPE(rust_psm_stack_pointer)
-FUNCTION(rust_psm_stack_pointer):
+FUNCTION(rust_psm_stack_pointer,0):
 /* extern "fastcall" fn() -> *mut u8 (%rax) */
 .cfi_startproc
     leal 4(%esp), %eax
@@ -47,10 +77,14 @@ SIZE(rust_psm_stack_pointer,.rust_psm_stack_pointer_end)
 .cfi_endproc
 
 
-GLOBL(rust_psm_replace_stack)
+COFFDEF(rust_psm_replace_stack,12)
+COFFSCL(2)
+COFFTYPE(32)
+COFFENDEF
+GLOBL(rust_psm_replace_stack,12)
 .p2align 4
 TYPE(rust_psm_replace_stack)
-FUNCTION(rust_psm_replace_stack):
+FUNCTION(rust_psm_replace_stack,12):
 /* extern "fastcall" fn(%ecx: usize, %edx: extern "fastcall" fn(usize), 4(%esp): *mut u8) */
 .cfi_startproc
 /*
@@ -70,10 +104,14 @@ SIZE(rust_psm_replace_stack,.rust_psm_replace_stack_end)
 .cfi_endproc
 
 
-GLOBL(rust_psm_on_stack)
+COFFDEF(rust_psm_on_stack,16)
+COFFSCL(2)
+COFFTYPE(32)
+COFFENDEF
+GLOBL(rust_psm_on_stack,16)
 .p2align 4
 TYPE(rust_psm_on_stack)
-FUNCTION(rust_psm_on_stack):
+FUNCTION(rust_psm_on_stack,16):
 /* extern "fastcall" fn(%ecx: usize, %edx: usize, 4(%esp): extern "fastcall" fn(usize, usize), 8(%esp): *mut u8) */
 .cfi_startproc
     pushl %ebp
