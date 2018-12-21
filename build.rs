18fffd8423c143a423b797dc97308c8b2f6c1796
Simonas Kazlauskas <git@kazlauskas.me>
2018-12-21 23:48:01 +0200

    Add SPARC code

--- build.rs
+++ build.rs
@@ -1,21 +1,23 @@
 extern crate cc;
 
-fn find_assembly(arch: &str, endian: &str, env: &str) -> Option<&'static str> {
-    match (arch, endian, env) {
-        (_,            _,        "msvc") => None,
-        ("x86",        _,        _) => Some("src/arch/x86.s"),
-        ("x86_64",     _,        _) => Some("src/arch/x86_64.s"),
-        ("arm",        _,        _) => Some("src/arch/arm_aapcs.s"),
-        ("armv7",      _,        _) => Some("src/arch/arm_aapcs.s"),
-        ("thumbv6",    _,        _) => Some("src/arch/arm_aapcs.s"),
-        ("thumbv7",    _,        _) => Some("src/arch/arm_aapcs.s"),
-        ("aarch64",    _,        _) => Some("src/arch/aarch_aapcs64.s"),
-        ("powerpc",    _,        _) => Some("src/arch/powerpc32.s"),
-        ("powerpc64",  "little", _) => Some("src/arch/powerpc64_openpower.s"),
-        ("powerpc64",  _,        _) => Some("src/arch/powerpc64.s"),
-        ("s390x",      _,        _) => Some("src/arch/zseries_linux.s"),
-        ("mips",       _,        _) => Some("src/arch/mips_eabi.s"),
-        ("mips64",     _,        _) => Some("src/arch/mips64_eabi.s"),
+fn find_assembly(arch: &str, endian: &str, os: &str, env: &str) -> Option<&'static str> {
+    match (arch, endian, os, env) {
+        (_,            _,        _, "msvc") => None,
+        ("x86",        _,        _, _) => Some("src/arch/x86.s"),
+        ("x86_64",     _,        _, _) => Some("src/arch/x86_64.s"),
+        ("arm",        _,        _, _) => Some("src/arch/arm_aapcs.s"),
+        ("armv7",      _,        _, _) => Some("src/arch/arm_aapcs.s"),
+        ("thumbv6",    _,        _, _) => Some("src/arch/arm_aapcs.s"),
+        ("thumbv7",    _,        _, _) => Some("src/arch/arm_aapcs.s"),
+        ("aarch64",    _,        _, _) => Some("src/arch/aarch_aapcs64.s"),
+        ("powerpc",    _,        _, _) => Some("src/arch/powerpc32.s"),
+        ("powerpc64",  "little", _, _) => Some("src/arch/powerpc64_openpower.s"),
+        ("powerpc64",  _,        _, _) => Some("src/arch/powerpc64.s"),
+        ("s390x",      _,        _, _) => Some("src/arch/zseries_linux.s"),
+        ("mips",       _,        _, _) => Some("src/arch/mips_eabi.s"),
+        ("mips64",     _,        _, _) => Some("src/arch/mips64_eabi.s"),
+        ("sparc64",    _,        _, _) => Some("src/arch/sparc64.s"),
+        ("sparc",    _,          _, _) => Some("src/arch/sparc_sysv.s"),
         _ => None,
     }
 }
@@ -25,7 +27,7 @@ fn main() {
     let env = ::std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
     let os = ::std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     let endian = ::std::env::var("CARGO_CFG_TARGET_ENDIAN").unwrap();
-    let asm = if let Some(asm) = find_assembly(&arch, &endian, &env) {
+    let asm = if let Some(asm) = find_assembly(&arch, &endian, &os, &env) {
         asm
     } else {
         eprintln!("({}, {}) arch-env pair is not supported", arch, env);
