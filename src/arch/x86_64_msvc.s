878cac06f7e1008512cef36f2eaa178b524d50ed
Simonas Kazlauskas <git@kazlauskas.me>
2018-12-22 17:27:12 +0200

    Add assembly files for MSVC

---
+++ src/arch/x86_64_msvc.s
@@ -0,0 +1,42 @@
+public _rust_psm_replace_stack
+
+PUBLIC rust_psm_stack_direction
+PUBLIC rust_psm_stack_pointer
+PUBLIC rust_psm_replace_stack
+PUBLIC rust_psm_on_stack
+
+_TEXT SEGMENT
+
+; extern "sysv64" fn() -> u8 (%al)
+rust_psm_stack_direction PROC
+    mov al, STACK_DIRECTION_DSCENDING
+    ret
+rust_psm_stack_direction ENDP
+
+; extern "sysv64" fn() -> *mut u8 (%rax)
+rust_psm_stack_pointer PROC
+    lea rax, [rsp + 8]
+    ret
+rust_psm_stack_pointer ENDP
+
+; extern "sysv64" fn(%rdi: usize, %rsi: extern "sysv64" fn(usize), %rdx: *mut u8)
+rust_psm_replace_stack PROC
+    lea rsp, [rdx - 8]
+    jmp [rsi]
+rust_psm_replace_stack ENDP
+
+; extern "sysv64" fn(%rdi: usize, %rsi: usize, %rdx: extern "sysv64" fn(usize, usize), %rcx: *mut u8)
+rust_psm_on_stack PROC FRAME
+    push rbp
+    .pushreg rbp
+    mov rbp, rsp
+    .setframe rbp, 0
+    .endprolog
+    mov rsp, rcx
+    call [rdx]
+    mov rsp, rbp
+    pop rbp
+    ret
+rust_psm_on_stack ENDP
+
+_TEXT ENDS
---
+++ src/arch/x86_msvc.s
@@ -0,0 +1,39 @@
+.386
+.model flat
+
+PUBLIC @rust_psm_stack_direction@0
+PUBLIC @rust_psm_stack_pointer@0
+PUBLIC @rust_psm_replace_stack@12
+PUBLIC @rust_psm_on_stack@16
+
+_TEXT SEGMENT
+
+/* extern "fastcall" fn() -> u8 (%al) */
+@rust_psm_stack_direction@0 PROC
+    mov al, STACK_DIRECTION_DSCENDING
+    ret
+@rust_psm_stack_direction@0 ENDP
+
+/* extern "fastcall" fn() -> *mut u8 (%rax) */
+@rust_psm_stack_pointer@0 PROC
+    lea eax, [esp + 4]
+    ret
+@rust_psm_stack_pointer@0 ENDP
+
+/* extern "fastcall" fn(%ecx: usize, %edx: extern "fastcall" fn(usize), 4(%esp): *mut u8) */
+@rust_psm_replace_stack@12 PROC
+    mov esp, [esp + 4]
+    jmp [edx]
+@rust_psm_replace_stack@12 ENDP
+
+/* extern "fastcall" fn(%ecx: usize, %edx: usize, 4(%esp): extern "fastcall" fn(usize, usize), 8(%esp): *mut u8) */
+@rust_psm_on_stack@16 PROC FRAME
+    push ebp
+    mov ebp, esp
+    mov esp, [ebp + 12]
+    call [ebp + 8]
+    mov esp, ebp
+    pop ebp
+    ret 8
+@rust_psm_on_stack@16 ENDP
+
