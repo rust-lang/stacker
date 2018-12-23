8f276e13f8a807c36babda5566d43782f4b73ba6
Simonas Kazlauskas <git@kazlauskas.me>
2018-12-23 20:27:14 +0200

    Remove any support for windows and add a note why

--- build.rs
+++ build.rs
@@ -2,22 +2,22 @@ extern crate cc;
 
 fn find_assembly(arch: &str, endian: &str, os: &str, env: &str) -> Option<&'static str> {
     match (arch, endian, os, env) {
-        (_,            _,        _, "msvc") => None,
-        ("x86",        _,        _, _) => Some("src/arch/x86.s"),
-        ("x86_64",     _,        _, _) => Some("src/arch/x86_64.s"),
-        ("arm",        _,        _, _) => Some("src/arch/arm_aapcs.s"),
-        ("armv7",      _,        _, _) => Some("src/arch/arm_aapcs.s"),
-        ("thumbv6",    _,        _, _) => Some("src/arch/arm_aapcs.s"),
-        ("thumbv7",    _,        _, _) => Some("src/arch/arm_aapcs.s"),
-        ("aarch64",    _,        _, _) => Some("src/arch/aarch_aapcs64.s"),
-        ("powerpc",    _,        _, _) => Some("src/arch/powerpc32.s"),
-        ("powerpc64",  "little", _, _) => Some("src/arch/powerpc64_openpower.s"),
-        ("powerpc64",  _,        _, _) => Some("src/arch/powerpc64.s"),
-        ("s390x",      _,        _, _) => Some("src/arch/zseries_linux.s"),
-        ("mips",       _,        _, _) => Some("src/arch/mips_eabi.s"),
-        ("mips64",     _,        _, _) => Some("src/arch/mips64_eabi.s"),
-        ("sparc64",    _,        _, _) => Some("src/arch/sparc64.s"),
-        ("sparc",    _,          _, _) => Some("src/arch/sparc_sysv.s"),
+        (_,            _,        "windows", _) => None,
+        ("x86",        _,        _,         _) => Some("src/arch/x86.s"),
+        ("x86_64",     _,        _,         _) => Some("src/arch/x86_64.s"),
+        ("arm",        _,        _,         _) => Some("src/arch/arm_aapcs.s"),
+        ("armv7",      _,        _,         _) => Some("src/arch/arm_aapcs.s"),
+        ("thumbv6",    _,        _,         _) => Some("src/arch/arm_aapcs.s"),
+        ("thumbv7",    _,        _,         _) => Some("src/arch/arm_aapcs.s"),
+        ("aarch64",    _,        _,         _) => Some("src/arch/aarch_aapcs64.s"),
+        ("powerpc",    _,        _,         _) => Some("src/arch/powerpc32.s"),
+        ("powerpc64",  "little", _,         _) => Some("src/arch/powerpc64_openpower.s"),
+        ("powerpc64",  _,        _,         _) => Some("src/arch/powerpc64.s"),
+        ("s390x",      _,        _,         _) => Some("src/arch/zseries_linux.s"),
+        ("mips",       _,        _,         _) => Some("src/arch/mips_eabi.s"),
+        ("mips64",     _,        _,         _) => Some("src/arch/mips64_eabi.s"),
+        ("sparc64",    _,        _,         _) => Some("src/arch/sparc64.s"),
+        ("sparc",    _,          _,         _) => Some("src/arch/sparc_sysv.s"),
         _ => None,
     }
 }
@@ -30,7 +30,7 @@ fn main() {
     let asm = if let Some(asm) = find_assembly(&arch, &endian, &os, &env) {
         asm
     } else {
-        eprintln!("({}, {}) arch-env pair is not supported", arch, env);
+        eprintln!("Target {}-{}-{} is not supported", arch, os, env);
         ::std::process::abort();
     };
 
