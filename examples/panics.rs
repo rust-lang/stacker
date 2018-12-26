41312d8087cca5f3de9bdc807c00bd68397f05e4
Simonas Kazlauskas <git@kazlauskas.me>
2018-12-26 09:39:07 +0200

    Add two examples

---
+++ examples/panics.rs
@@ -0,0 +1,40 @@
+extern crate psm;
+
+use std::{alloc, panic};
+
+const CHAIN_DEPTH: usize = 64;
+const STACK_ALIGN: usize = 4096;
+// Generating backraces (because of RUST_BACKTRACE) create a few quite large frames, so it is
+// important, that all frames have sufficient amount of available memory to not run over the
+// stack...
+const FRAME_SIZE: usize = 4096 * 10;
+
+
+fn panic_chain(depth: usize) {
+    if depth == 0 {
+        panic!("full chain!");
+    } else {
+        unsafe {
+            let layout = alloc::Layout::from_size_align(FRAME_SIZE, STACK_ALIGN).unwrap();
+            let new_stack = alloc::alloc(layout);
+            assert!(!new_stack.is_null(), "allocations must succeed!");
+            let p = psm::on_stack(new_stack, FRAME_SIZE, || {
+                panic::catch_unwind(|| {
+                    panic_chain(depth - 1);
+                })
+            });
+            alloc::dealloc(new_stack, layout);
+            p.map_err(panic::resume_unwind).unwrap()
+        }
+    }
+}
+
+
+fn main() {
+    panic_chain(CHAIN_DEPTH);
+}
+
+#[test]
+fn run_example() {
+    panic_chain(CHAIN_DEPTH);
+}
