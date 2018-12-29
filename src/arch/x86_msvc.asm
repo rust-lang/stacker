f44635b49619e2f7560879bad60a0cf4168e92b1
Simonas Kazlauskas <git@kazlauskas.me>
2018-12-29 21:53:17 +0200

    Fix x86 symbol mangling

--- src/arch/x86_msvc.asm
+++ src/arch/x86_msvc.asm
@@ -1,33 +1,36 @@
 .386
-.model flat,c
+.model flat
 
-PUBLIC @rust_psm_stack_direction@0
-PUBLIC @rust_psm_stack_pointer@0
-PUBLIC @rust_psm_replace_stack@12
-PUBLIC @rust_psm_on_stack@16
+; WTF: PUBLIC conflicts with "SYSCALL" but "SYSCALL" is the only way to stop MASM from manging the
+; symbol names?
+;
+; PUBLIC @rust_psm_stack_direction@0
+; PUBLIC @rust_psm_stack_pointer@0
+; PUBLIC @rust_psm_replace_stack@12
+; PUBLIC @rust_psm_on_stack@16
 
 _TEXT SEGMENT
 
 ; extern "fastcall" fn() -> u8 (%al)
-@rust_psm_stack_direction@0 PROC
+@rust_psm_stack_direction@0 PROC SYSCALL
     mov al, 2
     ret
 @rust_psm_stack_direction@0 ENDP
 
 ; extern "fastcall" fn() -> *mut u8 (%rax)
-@rust_psm_stack_pointer@0 PROC
+@rust_psm_stack_pointer@0 PROC SYSCALL
     lea eax, [esp + 4]
     ret
 @rust_psm_stack_pointer@0 ENDP
 
 ; extern "fastcall" fn(%ecx: usize, %edx: extern "fastcall" fn(usize), 4(%esp): *mut u8)
-@rust_psm_replace_stack@12 PROC
+@rust_psm_replace_stack@12 PROC SYSCALL
     mov esp, [esp + 4]
     jmp edx
 @rust_psm_replace_stack@12 ENDP
 
 ; extern "fastcall" fn(%ecx: usize, %edx: usize, 4(%esp): extern "fastcall" fn(usize, usize), 8(%esp): *mut u8)
-@rust_psm_on_stack@16 PROC FRAME
+@rust_psm_on_stack@16 PROC SYSCALL FRAME
     push ebp
     mov ebp, esp
     mov esp, [ebp + 12]
