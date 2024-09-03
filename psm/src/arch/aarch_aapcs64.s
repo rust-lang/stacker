e10b08d439dac08d115f41fd6ce648bbe2d7e1f3
Colin D Bennett <colin@gibibit.com>
2024-09-03 11:09:55 -0700

    fix: avoid macOS linker error for rust_psm_on_stack_end
    On macOS targets, don't define the symbol `rust_psm_on_stack_end`
    since it isn't used (and somehow this causes the build to fail).
    
    Fixes #87

--- psm/src/arch/aarch_aapcs64.s
+++ psm/src/arch/aarch_aapcs64.s
@@ -7,13 +7,14 @@
 #define GLOBL(fnname) .globl _##fnname
 #define TYPE(fnname)
 #define FUNCTION(fnname) _##fnname
-#define SIZE(fnname,endlabel)
+#define END_FUNCTION(fnname)
 
 #elif CFG_TARGET_OS_windows
 
 #define GLOBL(fnname) .globl fnname
 #define TYPE(fnname)
 #define FUNCTION(fnname) fnname
+#define LABEL_FOR_SIZE(endlabel)
 #define SIZE(fnname,endlabel)
 
 #else
@@ -21,7 +22,7 @@
 #define GLOBL(fnname) .globl fnname
 #define TYPE(fnname) .type fnname,@function
 #define FUNCTION(fnname) fnname
-#define SIZE(fnname,endlabel) .size fnname,endlabel-fnname
+#define END_FUNCTION(fnname) .size fnname,.-fnname
 
 #endif
 
@@ -34,8 +35,7 @@ FUNCTION(rust_psm_stack_direction):
 .cfi_startproc
     orr w0, wzr, #STACK_DIRECTION_DESCENDING
     ret
-.rust_psm_stack_direction_end:
-SIZE(rust_psm_stack_direction,.rust_psm_stack_direction_end)
+END_FUNCTION(rust_psm_stack_direction)
 .cfi_endproc
 
 
@@ -47,8 +47,7 @@ FUNCTION(rust_psm_stack_pointer):
 .cfi_startproc
     mov x0, sp
     ret
-.rust_psm_stack_pointer_end:
-SIZE(rust_psm_stack_pointer,.rust_psm_stack_pointer_end)
+END_FUNCTION(rust_psm_stack_pointer)
 .cfi_endproc
 
 
@@ -61,8 +60,7 @@ FUNCTION(rust_psm_replace_stack):
 /* All we gotta do is set the stack pointer to %rdx & tail-call the callback in %rsi */
     mov sp, x2
     br x1
-.rust_psm_replace_stack_end:
-SIZE(rust_psm_replace_stack,.rust_psm_replace_stack_end)
+END_FUNCTION(rust_psm_replace_stack)
 .cfi_endproc
 
 
@@ -87,6 +85,5 @@ FUNCTION(rust_psm_on_stack):
     .cfi_restore x29
     .cfi_restore x30
     ret
-.rust_psm_on_stack_end:
-SIZE(rust_psm_on_stack,.rust_psm_on_stack_end)
+END_FUNCTION(rust_psm_on_stack)
 .cfi_endproc
