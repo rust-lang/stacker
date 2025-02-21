61411cfe169c426aed5104967ab2a60244ca0171
Simonas Kazlauskas <git@kazlauskas.me>
2025-02-21 14:52:48 +0200

    stacker: 0.1.19

--- src/backends/unix.rs
+++ src/backends/unix.rs
@@ -5,26 +5,19 @@ use libc::pthread_getattr_np as get_attr;
 
 pub unsafe fn guess_os_stack_limit() -> Option<usize> {
     let mut attr = PthreadAttr::new()?;
-
-    handle_pthread_err(get_attr(libc::pthread_self(), attr.as_mut_ptr()))?;
-
+    (get_attr(libc::pthread_self(), &mut attr.0) == 0).then_some(())?;
     let mut stackaddr = std::ptr::null_mut();
     let mut stacksize = 0;
-    handle_pthread_err(libc::pthread_attr_getstack(
-        attr.as_mut_ptr(),
-        &mut stackaddr,
-        &mut stacksize,
-    ))?;
-
+    (libc::pthread_attr_getstack(&attr.0, &mut stackaddr, &mut stacksize) == 0).then_some(())?;
     Some(stackaddr as usize)
 }
 
-struct PthreadAttr(std::mem::MaybeUninit<libc::pthread_attr_t>);
+struct PthreadAttr(libc::pthread_attr_t);
 
 impl Drop for PthreadAttr {
     fn drop(&mut self) {
         unsafe {
-            let ret = libc::pthread_attr_destroy(self.0.as_mut_ptr());
+            let ret = libc::pthread_attr_destroy(&mut self.0);
             if ret != 0 {
                 let err = std::io::Error::last_os_error();
                 panic!(
@@ -36,23 +29,12 @@ impl Drop for PthreadAttr {
     }
 }
 
-fn handle_pthread_err(ret: libc::c_int) -> Option<()> {
-    if ret != 0 {
-        return None;
-    }
-    Some(())
-}
-
 impl PthreadAttr {
     unsafe fn new() -> Option<Self> {
         let mut attr = std::mem::MaybeUninit::<libc::pthread_attr_t>::uninit();
         if libc::pthread_attr_init(attr.as_mut_ptr()) != 0 {
             return None;
         }
-        Some(PthreadAttr(attr))
-    }
-
-    fn as_mut_ptr(&mut self) -> *mut libc::pthread_attr_t {
-        self.0.as_mut_ptr()
+        Some(PthreadAttr(attr.assume_init()))
     }
 }
