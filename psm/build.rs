fc990744ebbf4872401fbafcc3f37bc99a41712a
ntjohnson1 <24689722+ntjohnson1@users.noreply.github.com>
2025-08-12 09:21:01 -0400

    Downgrade ar_archiver to meet rust 1.63 requirements

--- psm/build.rs
+++ psm/build.rs
@@ -110,8 +110,8 @@ fn main() {
     // directly to `ar` to assemble an archive. Otherwise we're actually
     // compiling the source assembly file.
     if asm.ends_with(".o") {
-        use ar_archive_writer::{write_archive_to_stream, NewArchiveMember, ArchiveKind, DEFAULT_OBJECT_READER};
-        use std::fs::read;
+        use ar_archive_writer::{write_archive_to_stream, NewArchiveMember, ArchiveKind};
+        use std::fs::{read, metadata};
         use std::path::PathBuf;
         use std::io::Cursor;
 
@@ -119,23 +119,42 @@ fn main() {
         let output_path = PathBuf::from(&out_dir).join("libpsm_s.a");
 
         let object_data = read(asm).expect("Failed to read object file");
+        let file_metadata = metadata(asm).expect("Failed to read file metadata");
+
+        // Extract file metadata
+        let mtime = file_metadata.modified().ok()
+            .and_then(|time| time.duration_since(std::time::UNIX_EPOCH).ok())
+            .map(|duration| duration.as_secs())
+            .unwrap_or(0);
+
+        #[cfg(unix)]
+        let (uid, gid, perms) = {
+            use std::os::unix::fs::MetadataExt;
+            (file_metadata.uid(), file_metadata.gid(), file_metadata.mode())
+        };
+
+        #[cfg(not(unix))]
+        let (uid, gid, perms) = (0, 0, 0o644);
 
         let filename = asm.rsplit('/').next().unwrap_or(asm);
-        let member = NewArchiveMember::new(
-            object_data,
-            &DEFAULT_OBJECT_READER,
-            filename.to_string(),
-        );
+        let member = NewArchiveMember {
+            buf: Box::new(object_data),
+            get_symbols: |_data: &[u8], _callback: &mut dyn FnMut(&[u8]) -> Result<(), std::io::Error>| Ok(true),
+            member_name: filename.to_string(),
+            mtime,
+            uid,
+            gid,
+            perms,
+        };
 
         let mut output_bytes = Cursor::new(Vec::new());
         write_archive_to_stream(
             &mut output_bytes,
             &[member],
-            // Unfortunately, getDefaultKind() is not available in ar_archive_writer 0.4.2
+            // Unfortunately, getDefaultKind() is not available in ar_archive_writer
             // however looking at the llvm-ar source it looks like for wasm32-any-any
             // it falls through to Gnu https://llvm.org/doxygen/Object_2Archive_8cpp_source.html
             ArchiveKind::Gnu,
-            false,
             false,
         ).expect("Failed to write archive");
 
